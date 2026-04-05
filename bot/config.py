5ab79d498df2d6092dd9533f3246f83a029541b2
shinubo28always <shinubo28always@gmail.com>
2026-04-06 01:47:11 +0600

    Create config.py

---
+++ bot/config.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    API_ID = int(os.environ.get("API_ID", 0))
+    API_HASH = os.environ.get("API_HASH", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    MONGO_URI = os.environ.get("MONGO_URI", "")
+    OWNER_ID = int(os.environ.get("OWNER_ID", 0))
+    
+    # Anilist API URL
+    ANILIST_URL = "https://graphql.anilist.co"
